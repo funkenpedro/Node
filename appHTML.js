a41d31f3cfc34d7fb54b0bf6ad6699047d7d46c1
pedro <pedro@ncf.ca>
2020-04-02 22:27:06 -0400

    slightly better http server, next is to learn express

--- appHTML.js
+++ appHTML.js
@@ -1,6 +1,15 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, response) => {
+  if (req.url === "/") {
+    response.write("hello world");
+    response.end();
+  }
+  if (req.url === "/api") {
+    response.write(JSON.stringify([1, 2, 3]));
+    response.end();
+  }
+});
 //documentation shows http.server
 //inherits from net.Server
 //which is an eventEmitter
